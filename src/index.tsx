7b3c1b27cd98f9d9760b94fd06f229cdeece503c
anxiubin <tnqls0120@naver.com>
2021-06-14 17:55:06 +0900

    ✨ CRA로 프로젝트 세팅 및 router 세팅

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter } from "react-router-dom"
+import ReactDOM from "react-dom"
+import "antd/dist/antd.css"
+
+import Router from "./router"
+
+const App = () => (
+	<BrowserRouter>
+		<Router />
+	</BrowserRouter>
+)
+
+ReactDOM.render(<App />, document.getElementById("root"))
