30b730b044838dd93041d43c097b59672dcb9c65
anxiubin <tnqls0120@naver.com>
2021-06-15 19:28:24 +0900

    ✨ 특정 위치에서 Header 컴포넌트의 position을 fixed 로 변경

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,61 @@
+import { useEffect, useState } from "react"
 import { Row } from "antd"
+import clsx from "clsx"
 import { withTranslation } from "react-i18next"
 import Container from "../../common/Container"
+import { Button } from "../../common/Button"
 import { PngImg } from "../../common/PngImg"
-import { HeaderSection, LogoContainer } from "./styles"
+import { HeaderSection, LogoContainer, ButtonWrapper } from "./styles"
+import { getScroll } from "../../common/utils/getWindow"
+import Contents from "../../locales/ko/translation.json"
 
 const Header = ({ t }: any) => {
+	const [fixHeader, setFixHeader] = useState(false)
+
+	const checkScrollTop = (event: any) => {
+		const offsetFromTop = getScroll(event.target, true)
+
+		if (!fixHeader && offsetFromTop > 600) {
+			setFixHeader(true)
+		} else {
+			setFixHeader(false)
+		}
+	}
+
+	const onClick = () => {
+		window.open(
+			"https://chrome.google.com/webstore/detail/urlink/eimpopfllbjbhgkgomhhpolhlpaapdai?hl=ko"
+		)
+	}
+
+	useEffect(() => {
+		window.addEventListener("scroll", checkScrollTop)
+		return () => {
+			window.removeEventListener("scroll", checkScrollTop)
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [])
+
 	return (
-		<HeaderSection>
+		<HeaderSection
+			className={clsx({
+				fixed: Boolean(fixHeader),
+			})}
+		>
 			<Container>
 				<Row justify="space-between">
 					<LogoContainer to="/" aria-label="homepage">
 						<PngImg src="logo_full.png" width="108px" />
 					</LogoContainer>
+					<ButtonWrapper
+						className={clsx({
+							show: Boolean(fixHeader),
+						})}
+					>
+						<Button type="small" onClick={onClick}>
+							{t(Contents.getStartedSection.button)}
+						</Button>
+					</ButtonWrapper>
 				</Row>
 			</Container>
 		</HeaderSection>
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -2,7 +2,17 @@ import styled from "styled-components"
 import { Link } from "react-router-dom"
 
 export const HeaderSection = styled("header")`
-	padding: 3rem 0;
+	padding: 1rem 0;
+
+	&.fixed {
+		position: fixed;
+		top: 0;
+		left: 0;
+		background-color: #fff;
+		width: 100%;
+		z-index: 9999;
+		transition: all 0.3s ease-in-out;
+	}
 
 	.ant-row-space-between {
 		align-items: center;
@@ -17,4 +27,10 @@ export const LogoContainer = styled(Link)`
 export const NavLink = styled("div")`
 	display: inline-block;
 	text-align: center;
+`
+export const ButtonWrapper = styled("div")`
+	display: none;
+	&.show {
+		display: block;
+	}
 `
